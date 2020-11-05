d16e2ba109c3a914466041b58fe32af62de51bbb
Devin <devin.smith.work@gmail.com>
2020-11-05 07:58:23 -0800

    Add files via upload

---
+++ songData.js
@@ -0,0 +1,279 @@
+var songData =
+[{
+"Artist":"Ace of Base",
+"Title":"The Sign"
+},
+{
+"Artist":"Angelo Badalamenti",
+"Title":"Theme From Twin Peaks"
+},
+{
+"Artist":"Blondie",
+"Title":"Rapture",
+"Credit":"Sequenced by ChartChai MeeSangNin (jeridu@yahoo.com). Optimised for Roland SC-88 Pro or equivalents. To Ploy, Lassie, Farong, Jikko, Choco and White. Last revised: August 2001. Homepage: ttp://members.tripod.com/jeridu/"
+},
+{
+"Artist":"Bobby Brown",
+"Title":"Every Little Step You Take"
+},
+{
+"Artist":"Boyz II Men",
+"Title":"End Of The Road"
+},
+{
+"Artist":"Bruce Hornsby",
+"Title":"The Way It Is",
+"Credit":"Programmed by Hands On."
+},
+{
+"Artist":"Carly Simon",
+"Title":"Nobody Does It Better"
+},
+{
+"Artist":"Carly Simon",
+"Title":"Give Me All Night",
+"Credit":"Seq by Michael Boyce"
+},
+{
+"Artist":"The Carpenters",
+"Title":"Rainy Days And Mondays"
+},
+{
+"Artist":"Carole King",
+"Title":"So Far Away",
+"Credit":"© MIDI SONGS"
+},
+{
+"Artist":"The Commodores",
+"Title":"Sweet Love",
+"Credit":"© 1994 Tune 1000 Corporation"
+},
+{
+"Artist":"Crowded House",
+"Title":"Don't Dream It's Over",
+"Credit":"©1993 Edirol Italy"
+},
+{
+"Artist":"Destiny's Child",
+"Title":"Independent Woman Part 1"
+},
+{
+"Artist":"Destiny's Child",
+"Title":"No No No"
+},
+{
+"Artist":"Dire Straits",
+"Title":"Sultans of Swing",
+"Credit":"©1995 Backbeat Studio"
+},
+{
+"Artist":"Donald Fagen",
+"Title":"IGY"
+},
+{
+"Artist":"Doobie Brothers",
+"Title":"Long Train Running"
+},
+{
+"Artist":"Doobie Brothers",
+"Title":"Minute by Minute"
+},
+{
+"Artist":"En Vogue",
+"Title":"My Lovin'"
+},
+{
+"Artist":"Enya",
+"Title":"Sail Away"
+},
+{
+"Artist":"Fleetwood Mac",
+"Title":"Gypsy"
+},
+{
+"Artist":"Frankie Avalon",
+"Title":"Beauty School Dropout"
+},
+{
+"Artist":"Gerry Rafferty",
+"Title":"Baker Street (Remix)"
+},
+{
+"Artist":"Gerry Rafferty",
+"Title":"Right Down The Line"
+},
+{
+"Artist":"Henry Mancini",
+"Title":"Two For The Road"
+},
+{
+"Artist":"INXS",
+"Title":"Need You Tonight"
+},
+{
+"Artist":"Janet Jackson",
+"Title":"Escapade"
+},
+{
+"Artist":"Jennier Page",
+"Title":"Crush"
+},
+{
+"Artist":"Johnny Mathis",
+"Title":"When Sunny Gets Blue",
+"Credit":"© 1993 Bob Mace"
+},
+{
+"Artist":"Lionel Richie",
+"Title":"Easy",
+"Credit":"© 1994 Tune 1000 Corporation"
+},
+{
+"Artist":"Lionel Richie",
+"Title":"All Night Long"
+},
+{
+"Artist":"Lou Rawls",
+"Title":"You'll Never Find Another Love Like Mine",
+"Credit":"MIDI C.A.D. Productions Inc, midicad@yahoo.com"
+},
+{
+"Artist":"Mariah Carey",
+"Title":"Fantasy"
+},
+{
+"Artist":"Mariah Carey",
+"Title":"Dreamlover",
+"Credit":"Arranged by Alan Lok, From The Clubhouse BBS (416)754-2556, EMail: alok@yonet.org, aj707@torfree.org"
+},
+{
+"Artist":"Michel Legrand",
+"Title":"Windmills Of Your Mind",
+"Credit":"© 2008, arr. for 2 Guitars & Orchestra (x Tape) by Serban Nichifor dwmzvrsn 6.0.12.0"
+},
+{
+"Artist":"Phil Collins",
+"Title":"Sussudio"
+},
+{
+"Artist":"Prince",
+"Title":"Diamonds And Pearls",
+"Credit":"©1994 International Music Publications Ltd ©1994 Barnes Music Engraving"
+},
+{
+"Artist":"Roxy Music",
+"Title":"More Than This",
+"Credit":"©1998 by M-LIVE s.r.l."
+},
+{
+"Artist":"Sade",
+"Title":"Your Love Is King",
+"Credit":"©1995 by MdB Software"
+},
+{
+"Artist":"Sade",
+"Title":"Smooth Operator"
+},
+{
+"Artist":"Sade",
+"Title":"Sweetest Taboo",
+"Credit":"Sequenced by Johannes Karhula 5/1994"
+},
+{
+"Artist":"Savage Garden",
+"Title":"Truly Madly Deeply",
+"Credit":"Cascada - Copyright © by MIDI-NET s.c., Wszelkie prawa zastrzezone!!! 2007, Ten plik MIDI zostal napisany przez muzykow firmy MIDI-NET!!! Kopiowanie surowo zabronione!!!"
+},
+{
+"Artist":"Spandau Ballet",
+"Title":"True"
+},
+{
+"Artist":"The Spinners",
+"Title":"Could It Be I'm Falling In Love?",
+"Filename":"Could It Be I'm Falling In Love",
+"Credit":"MIDI C.A.D. Productions Inc, midicad@yahoo.com"
+},
+{
+"Artist":"Starship",
+"Title":"Sara"
+},
+{
+"Artist":"Steely Dan",
+"Title":"Bad Sneakers"
+},
+{
+"Artist":"Steely Dan",
+"Title":"Doctor Wu"
+},
+{
+"Artist":"Steely Dan",
+"Title":"Gaslighting Abbie"
+},
+{
+"Artist":"Steely Dan",
+"Title":"FM (No Static At All)",
+"Credit":"Karaoke by Raphael Pungin (raphael@cnct.com)"
+},
+{
+"Artist":"Steely Dan",
+"Title":"Babylon Sisters",
+"Credit":"Seq by Stein Rimehaug Waystein@aol.com"
+},
+{
+"Artist":"Steve Winwood",
+"Title":"Higher Love",
+"Credit":"Soft karaoke"
+},
+{
+"Artist":"Sting",
+"Title":"If I Ever Lose My Faith In You",
+"Credit":"©1998 by M-LIVE s.r.l., WWW.OKYWEB.COM"
+},
+{
+"Artist":"Sting",
+"Title":"Fields Of Gold",
+"Credit":"©1993 Music Sales Ltd."
+},
+{
+"Artist":"Sting",
+"Title":"Englishman In New York",
+"Credit":"©1993 Music Sales Ltd."
+},
+{
+"Artist":"The Stylistics",
+"Title":"Betcha By Golly Wow"
+},
+{
+"Artist":"The Stylistics",
+"Title":"Stoned In Love With You",
+"Credit":"MIDI C.A.D. Productions Inc, midicad@yahoo.com"
+},
+{
+"Artist":"Tina Turner",
+"Title":"What's Love Got To Do With it?",
+"Filename":"What's Love Got To Do With it",
+"Credit":"© 1994 Heavenly Music"
+},
+{
+"Artist":"TLC",
+"Title":"Waterfalls"
+},
+{
+"Artist":"Toni Braxton",
+"Title":"Unbreak My Heart",
+"Credit":"©1995 by Jon B. Kusaka"
+},
+{
+"Artist":"Vanessa Williams",
+"Title":"Save The Best For Last"
+},
+{
+"Artist":"Wham!",
+"Title":"Club Tropicana"
+},
+{
+"Artist":"Yes",
+"Title":"Roundabout",
+"Credit":"TUNE 1000 CORP."
+}]
